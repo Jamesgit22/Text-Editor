ce405a22fdea128e87c412bd9411c44978ba90b4
Jamesgit22 <jschroeder97@icloud.com>
2023-05-23 16:11:24 -0500

    added code to starter code

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,19 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// May need to be refactored
+registerRoute(
+  ({ request }) => request.destination === 'assets',
+  new CacheFirst({
+    cacheName: 'textEv1',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
